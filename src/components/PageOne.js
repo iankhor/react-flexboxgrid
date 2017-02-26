984c2c5bacc37f34930b4bd49f2c310248d7da08
iankhor <iankhorbc.thecoder@gmail.com>
2017-02-26 19:47:20 +1100

    updated readme and pageone

--- src/components/PageOne.js
+++ src/components/PageOne.js
@@ -19,53 +19,58 @@ class PageOne extends Component {
                 { /* 2 row 2 columns 3 columns */ }
                 <div className="row">
                     <div className="col-xs-4">
-                        <div className="box color-white border">Row 1 Column 1</div>
+                        <div className="box color-white border">Non responsive Column 1</div>
                     </div>
 
                     <div className="col-xs-4">
-                        <div className="box color-white border">Row 1 Column 2</div>
+                        <div className="box color-white border">Non responsive Column 2</div>
                     </div>
 
                     <div className="col-xs-4">
-                        <div className="box color-white border">Row 1 Column 3</div>
+                        <div className="box color-white border">Non responsive Column 3</div>
                     </div>
                 </div>
+
+                <br />
 
                 <div className="row">
                     <div className="col-xs-6">
-                        <div className="box color-pink border">Row 2 Column 1</div>
+                        <div className="box color-pink border">Non responsive Column 1</div>
                     </div>
 
                     <div className="col-xs-6">
-                        <div className="box color-pink border">Row 2 Column 2</div>
+                        <div className="box color-pink border">Non responsive Column 2</div>
                     </div>
                 </div>
 
+                <br />
 
-                { /* Responsive 2 column */ }
-                <div className="row">
-                    <div className="col-xs-12 col-sm-8 col-md-6 col-lg-4">
-                        <div className="box color-white">Responsive 1</div>
-                    </div>
-                    <div className="col-xs-12 col-sm-8 col-md-6 col-lg-4">
-                        <div className="box color-white">Responsive 2</div>
-                    </div>
-                </div>
 
                 { /* Responsive 3 column */ }
                 <div className="row">
                     <div className="col-xs-12 col-sm-8 col-md-4 col-lg-4">
-                        <div className="box color-pink">Responsive 1</div>
+                        <div className="box color-white border">Responsive Column 1</div>
                     </div>
                     <div className="col-xs-12 col-sm-8 col-md-4 col-lg-4">
-                        <div className="box color-pink">Responsive 2</div>
+                        <div className="box color-white border">Responsive Column 2</div>
                     </div>
 
                     <div className="col-xs-12 col-sm-8 col-md-4 col-lg-4">
-                        <div className="box color-pink">Responsive 3</div>
+                        <div className="box color-white border">Responsive Column 3</div>
                     </div>
                 </div>
                 
+                <br />
+                
+                { /* Responsive 2 column */ }
+                <div className="row">
+                    <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                        <div className="box color-pink border">Responsive Column 1</div>
+                    </div>
+                    <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                        <div className="box color-pink border">Responsive Column 2</div>
+                    </div>
+                </div>
 
             </App> 
         )
